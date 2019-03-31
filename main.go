4cf873f8829fe19bef38c4af894f4aadd58c13eb
yonseca <yonseca@gmail.com>
2019-04-01 00:42:52 +0200

    Methods for building result

--- main.go
+++ main.go
@@ -25,23 +25,7 @@ func getBusStopsInfo(w http.ResponseWriter, r *http.Request) {
 	request.Header.Add("Accept", "application/json")
 	resp, err := client.Do(request)
 	if err != nil {
-		fmt.Println("fail!")
-		return
-	}
-	defer resp.Body.Close()
-	a, _ := ioutil.ReadAll(resp.Body)
-	fmt.Printf("%s", a)
-}
-
-// getUrbanBusStopInfo Gets all the information for a specific urban bus stop
-func getUrbanBusStopInfo(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", "https://www.zaragoza.es/sede/servicio/urbanismo-infraestructuras/transporte-urbano/poste-autobus/tuzsa-" + params["id"] + "?rf=html&srsname=wgs84", nil)
-	request.Header.Add("Accept", "application/json")
-	resp, err := client.Do(request)
-	if err != nil {
-		fmt.Println("fail!")
+		buildResultFail(w)
 		return
 	}
 	defer resp.Body.Close()
@@ -49,7 +33,34 @@ func getUrbanBusStopInfo(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("%s", a)
 
 	// Write results
+	buildResultOK(w, a)
+}
+
+// getUrbanBusStopInfo Gets all the information for a specific urban bus stop
+func getUrbanBusStopInfo(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", "https://www.zaragoza.es/sede/servicio/urbanismo-infraestructuras/transporte-urbano/poste-autobus/tuzsa-"+params["id"]+"?rf=html&srsname=wgs84", nil)
+	request.Header.Add("Accept", "application/json")
+	resp, err := client.Do(request)
+	if err != nil {
+		buildResultFail(w)
+		return
+	}
+	defer resp.Body.Close()
+	a, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("%s", a)
+	// Write results
+	buildResultOK(w, a)
+}
+
+func buildResultOK(w http.ResponseWriter, a []byte) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(a)
+}
+
+func buildResultFail(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
 }
